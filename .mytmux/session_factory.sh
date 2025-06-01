17c46f4db835639de7c3bc2c125bc518b969c787
K-R-S-NULL <94648224+K-R-S-NULL@users.noreply.github.com>
2025-06-01 23:21:10 +0200

    Update session_factory.sh
    fix

--- .mytmux/session_factory.sh
+++ .mytmux/session_factory.sh
@@ -1,37 +1,40 @@
 #!/bin/env bash
+source ~/.mytmux/mytmux_core.sh
 
 facotrySimpleSession(){
     SESSIONNAME=$1
-    tmux has-session -t $SESSIONNAME
-    if [ $? -eq 0 ]; then
-        echo "Session $SESSIONNAME already exists."
-    else
-        tmux new-session -d -s $SESSIONNAME
-        tmux attach -t $SESSIONNAME
-        tmux switch -t $SESSIONNAME
-    fi
+    createSession $SESSIONNAME
+    tmux switch -t $SESSIONNAME
 }
 
 factorySSHConnection(){
     SESSIONNAME=$1
     SSHHOST=$2
     tmux has-session -t $SESSIONNAME
-    if [ $? -eq 0 ]; then
-        echo "Session $SESSIONNAME already exists."
-    else
-        tmux new-session -d -s $SESSIONNAME
+    if [ $? -ne 0 ]; then
+        createSession $SESSIONNAME
+        tmux rename-window -t $SESSIONNAME "ssh ${SSHHOST}"
         tmux send-keys -t $SESSIONNAME "ssh ${SSHHOST}" C-m
-        tmux attach -t $SESSIONNAME
-        tmux switch -t $SESSIONNAME
     fi
+    tmux switch -t $SESSIONNAME
 }
 
 factoryMultiSSHConnection(){
     SESSIONNAME=$1
     SSHHOST=$2
-    CONNECTIONS=$(($3))
-    echo "$SESSIONNAME"
-    echo "$SSHHOST"
-    echo "$CONNECTIONS"
-    sleep 10
+    CONNECTIONS=$(((($3))-1))
+    tmux has-session -t $SESSIONNAME
+    if [ $? -ne 0 ]; then
+        createSession $SESSIONNAME
+        tmux rename-window -t $SESSIONNAME "ssh ${SSHHOST} [${3} times]"
+        tmux select-pane -t $SESSIONNAME:0 -T "ssh ${SSHHOST}"
+        tmux send-keys -t $SESSIONNAME "ssh ${SSHHOST}" C-m
+        for i in $(seq 0 $(($CONNECTIONS-1))); do
+            tmux split-window -t $SESSIONNAME -v
+            tmux select-pane -t $SESSIONNAME:0 -T "ssh ${SSHHOST}"
+            tmux send-keys -t $SESSIONNAME "ssh ${SSHHOST}" C-m
+        done
+    fi
+    tmux select-layout even-vertical
+    tmux switch -t $SESSIONNAME
 }
