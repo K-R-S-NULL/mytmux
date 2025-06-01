c5e9e599e46246b5e6ac79e31acd12376d86b5e2
K-R-S-NULL <94648224+K-R-S-NULL@users.noreply.github.com>
2025-06-01 15:10:25 +0200

    Create init_mytmux.sh

---
+++ .mytmux/init_mytmux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#set -x
+
+source ~/.mytmux/mytmux_core.sh
+TMUX="tmux -2 -f ~/.mytmux/tmux.conf"
+SESSIONNAME=tmux_menu
+
+MENU_SCRIPT=./menu_mytmux.sh
+
+$TMUX has-session -t $SESSIONNAME
+if [ $? -eq 0 ]; then
+       echo "Session $SESSIONNAME already exists. Attaching."
+       sleep 1
+       $TMUX attach -t $SESSIONNAME
+       exit 0;
+fi
+
+tmux new-session -d -s $SESSIONNAME
+# Attach to the created session
+
+tmux send-keys -t $SESSIONNAME 'eval "$(ssh-agent -s)"' C-m
+COMMAND="ssh-add ~/.ssh/ssh_user_ca && $MENU_SCRIPT"
+tmux send-keys -t $SESSIONNAME "$COMMAND" C-m
+
+tmux attach-session -t $SESSIONNAME
